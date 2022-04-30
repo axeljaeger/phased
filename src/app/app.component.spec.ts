e8a185995a29d6423756271c82893db853f6496e
axel.jaeger <axel.jaeger@arculus.de>
2022-04-30 14:49:11 +0200

    Fix unittests.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,16 +1,45 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { View3dComponent } from './view3d/view3d.component';
+import { MenuLeftComponent } from './menu-left/menu-left.component';
+import { MockComponent } from 'ng-mocks';
+
+
+import { FormBuilder } from '@angular/forms';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatIconModule } from '@angular/material/icon';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        MatExpansionModule,
+        MatListModule,
+        MatSidenavModule,
+        NoopAnimationsModule, 
+        MatButtonToggleModule,
+        MatIconModule,
+        MatFormFieldModule,
+        ReactiveFormsModule,
+        MatInputModule,
       ],
       declarations: [
-        AppComponent
+        AppComponent,
+        MenuLeftComponent,
+        MockComponent(View3dComponent)
       ],
+      providers: [
+        { provide: FormBuilder, useClass: FormBuilder}
+      ]
     }).compileComponents();
   });
 
@@ -26,10 +55,4 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('usarray');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('usarray app is running!');
-  });
 });
--- src/app/menu-left/menu-left.component.spec.ts
+++ src/app/menu-left/menu-left.component.spec.ts
@@ -2,6 +2,15 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormBuilder } from '@angular/forms';
 
 import { MenuLeftComponent } from './menu-left.component';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { NoopAnimationsModule  } from '@angular/platform-browser/animations';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+
 
 describe('MenuLeftComponent', () => {
   let component: MenuLeftComponent;
@@ -10,8 +19,18 @@ describe('MenuLeftComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ MenuLeftComponent ],
+      imports: [
+        MatExpansionModule,
+        MatButtonToggleModule,
+        MatFormFieldModule,
+        MatIconModule,
+        MatInputModule,
+        MatListModule,
+        NoopAnimationsModule,
+        ReactiveFormsModule
+      ],
       providers: [
-        { provide: FormBuilder, useValue: new FormBuilder()}
+        { provide: FormBuilder, useClass: FormBuilder}
       ]
     })
     .compileComponents();
