5df04680f014f1b11d3e308b429c431c2d2128a5
axel.jaeger <axel.jaeger@arculus.de>
2022-04-30 13:43:26 +0200

    Fix unittests for menu left.

--- src/app/menu-left/menu-left.component.spec.ts
+++ src/app/menu-left/menu-left.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormBuilder } from '@angular/forms';
 
 import { MenuLeftComponent } from './menu-left.component';
 
@@ -8,7 +9,10 @@ describe('MenuLeftComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ MenuLeftComponent ]
+      declarations: [ MenuLeftComponent ],
+      providers: [
+        { provide: FormBuilder, useValue: new FormBuilder()}
+      ]
     })
     .compileComponents();
   });
