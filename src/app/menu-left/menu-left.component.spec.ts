8a7f1b59045cc29995efb62ae83a4c13938df2fc
axel.jaeger <axeljaeger@gmail.com>
2022-05-07 17:17:56 +0200

    Move test metadata-definition out of unittest file.

--- src/app/menu-left/menu-left.component.spec.ts
+++ src/app/menu-left/menu-left.component.spec.ts
@@ -1,60 +1,13 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { provideMockStore, MockStore } from '@ngrx/store/testing';
-import { ReactiveComponentModule } from '@ngrx/component'
-
-import { FormBuilder } from '@angular/forms';
-
 import { MenuLeftComponent } from './menu-left.component';
-import { MatExpansionModule } from '@angular/material/expansion';
-import { MatButtonToggleModule } from '@angular/material/button-toggle';
-import { MatIconModule } from '@angular/material/icon';
-import { MatListModule } from '@angular/material/list';
-import { NoopAnimationsModule  } from '@angular/platform-browser/animations';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { ReactiveFormsModule } from '@angular/forms';
-import { MatInputModule } from '@angular/material/input';
-
+import * as menuLeft from './menu-left.metadata' ;
 
 describe('MenuLeftComponent', () => {
   let component: MenuLeftComponent;
   let fixture: ComponentFixture<MenuLeftComponent>;
 
-  const initialState = {
-    arrayConfig: {
-      arrayType: 'ura',
-      uraConfig: {
-        elementsX: 2,
-        elementsY: 2,
-        pitchX: 0.0043,
-        pitchY: 0.0043
-      },
-      circularConfig: {
-        radius: 2,
-        elements: 2,
-      }
-    }
-  };
-
   beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ MenuLeftComponent ],
-      imports: [
-        MatExpansionModule,
-        MatButtonToggleModule,
-        MatFormFieldModule,
-        MatIconModule,
-        MatInputModule,
-        MatListModule,
-        NoopAnimationsModule,
-        ReactiveFormsModule,
-        ReactiveComponentModule
-      ],
-      providers: [
-        { provide: FormBuilder, useClass: FormBuilder},
-        provideMockStore({ initialState }),
-
-      ]
-    })
+    await TestBed.configureTestingModule(menuLeft.moduleMetaData)
     .compileComponents();
   });
 
---
+++ src/app/menu-left/menu-left.metadata.ts
@@ -0,0 +1,49 @@
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { NoopAnimationsModule  } from '@angular/platform-browser/animations';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveComponentModule } from '@ngrx/component'
+
+import { MenuLeftComponent } from './menu-left.component';
+
+import { MatInputModule } from '@angular/material/input';
+import { provideMockStore } from '@ngrx/store/testing';
+import { FormBuilder } from '@angular/forms';
+
+export const initialState = {
+    arrayConfig: {
+      arrayType: 'ura',
+      uraConfig: {
+        elementsX: 2,
+        elementsY: 2,
+        pitchX: 0.0043,
+        pitchY: 0.0043
+      },
+      circularConfig: {
+        radius: 2,
+        elements: 2,
+      }
+    }
+  }; 
+
+export const moduleMetaData = {
+    declarations: [ MenuLeftComponent ],
+    imports: [
+      MatExpansionModule,
+      MatButtonToggleModule,
+      MatFormFieldModule,
+      MatIconModule,
+      MatInputModule,
+      MatListModule,
+      NoopAnimationsModule,
+      ReactiveFormsModule,
+      ReactiveComponentModule
+    ],
+    providers: [
+      {provide: FormBuilder, useClass: FormBuilder},
+      provideMockStore({ initialState }),  
+  ]
+};
