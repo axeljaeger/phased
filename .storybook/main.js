60955450c839257c17adb12b4687ce47730ee790
Axel Jäger <axel.jaeger@arculus.de>
2022-10-23 17:11:52 +0200

    Update storybook to v7

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,18 +1,12 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
-  ],
-  "framework": "@storybook/angular",
-  "core": {
-    "builder": "@storybook/builder-webpack5"
+  "stories": ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  "addons": ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions"],
+  "framework": {
+    name: "@storybook/angular",
+    options: {}
   },
+  "core": {},
   features: {
-    interactionsDebugger: true,
-  },
+    interactionsDebugger: true
+  }
 };
