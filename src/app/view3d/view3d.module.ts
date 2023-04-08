e9d999faf3f4cc4916afc4f9fe903cb108eb6623
Axel Jäger <axeljaeger@googlemail.com>
2023-04-08 12:38:49 +0200

    Add renderer to filename of excitation renderer.

--- src/app/view3d/view3d.module.ts
+++ src/app/view3d/view3d.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { BabylonSceneDirective } from './directives/babylon-scene.directive';
 import { TransducerBufferDirective } from './directives/transducer-buffer.directive';
 
-import { ExcitationRendererComponent } from './renderers/excitation/excitation.component';
+import { ExcitationRendererComponent } from './renderers/excitation/excitation-renderer.component';
 import { FarfieldRendererComponent } from './renderers/farfield/farfield-renderer.component';
 import { RayleighIntegralRendererComponent } from './renderers/rayleigh-integral/rayleigh-renderer.component';
 import { View3dComponent } from './smart-components/view3d/view3d.component';
