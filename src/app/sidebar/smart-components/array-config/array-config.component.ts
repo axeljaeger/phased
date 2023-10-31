03dc4716355041fcc5024e773cb5c06f48bc0191
Axel Jäger <axeljaeger@gmail.com>
2023-10-31 19:04:08 +0100

    Implement spiral and circular array.

--- src/app/sidebar/smart-components/array-config/array-config.component.ts
+++ src/app/sidebar/smart-components/array-config/array-config.component.ts
@@ -6,7 +6,7 @@ import { Store } from '@ngrx/store';
 import { ArrayConfig, ArrayConfigActions, arrayConfigFeature } from '../../../store/arrayConfig.state'
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { NgIf } from '@angular/common';
+import { NgSwitch, NgSwitchCase, NgSwitchDefault } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { MatExpansionModule } from '@angular/material/expansion';
@@ -16,7 +16,7 @@ import { MatExpansionModule } from '@angular/material/expansion';
     templateUrl: './array-config.component.html',
     styleUrls: ['./array-config.component.css'],
     standalone: true,
-    imports: [MatExpansionModule, ReactiveFormsModule, MatButtonToggleModule, MatIconModule, NgIf, MatFormFieldModule, MatInputModule]
+    imports: [MatExpansionModule, ReactiveFormsModule, MatButtonToggleModule, MatIconModule, NgSwitch, NgSwitchCase, NgSwitchDefault, MatFormFieldModule, MatInputModule]
 })
 export class ArrayConfigComponent implements OnInit {
   public arrayConfig: any;
@@ -34,9 +34,9 @@ export class ArrayConfigComponent implements OnInit {
         pitchX: this.fb.control(0),
         pitchY: this.fb.control(0),
       }),
-      circularConfig: this.fb.group({
-        radius: this.fb.control(0),
-        elements: this.fb.control(0),
+      roundConfig: this.fb.group({
+        diameter: this.fb.control(0),
+        elementCount: this.fb.control(0),
       }),
     });
     
