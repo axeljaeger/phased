1a0b8f5ff34488870f3e0f03c6059f28e3306a50
Axel Jäger <axeljaeger@gmail.com>
2024-08-16 16:11:47 +0200

    Mat3 design (#1128)
    * Use mat3 theme preset, fix sidebar boxes
    
    * Fix name of scss file.
    
    * Implement mat3 design

--- src/app/sidebar/pure-components/sidebar-container/sidebar-container.component.ts
+++ src/app/sidebar/pure-components/sidebar-container/sidebar-container.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { FarfieldComponent } from '../../smart-components/farfield/farfield.component';
 import { RayleighComponent } from '../../smart-components/rayleigh/rayleigh.component';
 import { TransducerListComponent } from '../../smart-components/transducer-list/transducer-list.component';
@@ -6,7 +6,13 @@ import { ArrayConfigComponent } from '../../smart-components/array-config/array-
 import { MatExpansionModule } from '@angular/material/expansion';
 import { ChartComponent } from '../../smart-components/chart/chart.component';
 import { BeamformingComponent } from '../../smart-components/beamforming/beamforming.component';
-
+import { Store } from '@ngrx/store';
+import { arrayConfigFeature } from 'src/app/store/arrayConfig.state';
+import { map } from 'rxjs/operators';
+import { AsyncPipe } from '@angular/common';
+import { beamformingFeature } from 'src/app/store/beamforming.state';
+import { MatIconModule } from '@angular/material/icon';
+import { Results, ViewportFeature } from 'src/app/store/viewportConfig.state';
 
 @Component({
     selector: 'app-sidebar-container',
@@ -14,13 +20,21 @@ import { BeamformingComponent } from '../../smart-components/beamforming/beamfor
     styleUrls: ['./sidebar-container.component.scss'],
     standalone: true,
     imports: [
+        AsyncPipe,
         ArrayConfigComponent, 
         ChartComponent,
         FarfieldComponent,
-        MatExpansionModule, 
+        MatExpansionModule,
+        MatIconModule, 
         RayleighComponent, 
         TransducerListComponent,
         BeamformingComponent
     ]
 })
-export class SidebarContainerComponent {}
+export class SidebarContainerComponent {
+    private store = inject(Store);
+    public transducersCount$ = this.store.select(arrayConfigFeature.selectTransducers).pipe(map(transducers => transducers.length));
+    public beamformingEnabled$ = this.store.select(beamformingFeature.selectEnabled);
+    public rayleighVisible$ = this.store.select(ViewportFeature.selectResultEnabled(Results.RayleighIntegral));
+    public farfieldVisible$ = this.store.select(ViewportFeature.selectResultEnabled(Results.Farfield));
+}
