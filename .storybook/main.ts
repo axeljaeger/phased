b21d9a210d6031adf7dbb5b9c92b1a0a44e5c7c0
Axel Jäger <axeljaeger@gmail.com>
2024-10-09 18:33:50 +0200

    Update deps, fix stories & storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,6 +1,7 @@
 import { StorybookConfig } from '@storybook/angular';
 const config: StorybookConfig = {
   "stories": ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  "staticDirs": ["../src"],
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
