45683f1457132d9598c2a852ba88d0b5ac2c44e5
Axel Jäger <axeljaeger@gmail.com>
2025-03-13 22:14:28 +0100

    Use router to switch sidebar content (#1423)

--- src/main.ts
+++ src/main.ts
@@ -13,10 +13,20 @@ import { beamformingFeature } from './app/store/beamforming.state';
 import { arrayConfigFeature } from './app/store/arrayConfig.state';
 import { exportFeature } from './app/store/export.state';
 import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
+import { provideRouter, Routes } from '@angular/router';
+import { ExportComponent } from './app/sidebar/pure-components/export/export.component';
+import { SetupContainerComponent } from './app/sidebar/smart-components/setup-container/setup-container.component';
+import { LibraryContainerComponent } from './app/sidebar/smart-components/library-container/library-container.component';
 
 if (environment.production) {
   enableProdMode();
 }
+
+const routes : Routes = [
+  { path: 'library', component: LibraryContainerComponent },
+  { path: 'setup', component: SetupContainerComponent },
+  { path: 'export', component: ExportComponent },
+];
 
 bootstrapApplication(AppComponent, {
     providers: [
@@ -25,12 +35,11 @@ bootstrapApplication(AppComponent, {
         provideStore(),
         provideState(arrayConfigFeature),
         provideState(beamformingFeature),
-        
         provideState(exportFeature),
-        
         provideState(RayleighFeature),
         provideState(selectionFeature),
         provideState(ViewportFeature),
+        provideRouter(routes),
         provideStoreDevtools({
             maxAge: 25,
             logOnly: environment.production, // Restrict extension to log-only mode
