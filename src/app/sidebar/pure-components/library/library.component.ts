29755563052fbe354603c498c5aafef51b8c123c
Axel Jäger <axeljaeger@gmail.com>
2025-07-29 19:59:04 +0200

    Cleanup library container

--- src/app/sidebar/pure-components/library/library.component.ts
+++ src/app/sidebar/pure-components/library/library.component.ts
@@ -1,11 +1,8 @@
-import { Component, output, inject, AfterViewInit, DestroyRef } from '@angular/core';
+import { Component, input } from '@angular/core';
 
-import { presets } from '../../../presets'
 import { MatSelectionList, MatListItem, MatListItemTitle, MatListItemIcon, MatListItemLine } from '@angular/material/list';
 import { MatIcon } from '@angular/material/icon';
-import { ActivatedRoute, RouterLink } from '@angular/router';
-import { map } from 'rxjs';
-import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { RouterLink } from '@angular/router';
 import { ArrayConfig } from 'src/app/store/store.service';
 
 @Component({
@@ -14,20 +11,6 @@ import { ArrayConfig } from 'src/app/store/store.service';
   templateUrl: './library.component.html',
   styleUrl: './library.component.scss'
 })
-export class LibraryComponent implements AfterViewInit {
-  destroyRef = inject(DestroyRef);
-  presets = presets;
-  loadPreset = output<ArrayConfig>();
-  
-  private route = inject(ActivatedRoute);
-  // FIXME: This should not be necessary due to router withComponentInputBinding
-  index$ = this.route.paramMap.pipe(map(params => params.get('libraryIndex')));
-
-  ngAfterViewInit(): void {
-    this.index$.pipe(takeUntilDestroyed(this.destroyRef)).subscribe(index => {
-      if (index !== null && index !== undefined) {
-        this.loadPreset.emit(this.presets[+index]);
-      }
-  });
-  }
+export class LibraryComponent {
+  presets = input<ArrayConfig[]>([]);
 }
