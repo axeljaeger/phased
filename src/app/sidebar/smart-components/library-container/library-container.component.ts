8fab50c43c0c9e038e3f60ae4e2b5dbb7ebe10a1
Axel Jäger <axeljaeger@gmail.com>
2025-07-29 19:59:19 +0200

    Cleanup library container (#1562)

--- src/app/sidebar/smart-components/library-container/library-container.component.ts
+++ src/app/sidebar/smart-components/library-container/library-container.component.ts
@@ -1,7 +1,8 @@
-import { Component, computed, inject } from '@angular/core';
+import { Component, computed, effect, inject, input } from '@angular/core';
 import { LibraryComponent } from '../../pure-components/library/library.component';
 import { InfoComponent } from '../../pure-components/info/info.component';
-import { ArrayConfig, StoreService } from 'src/app/store/store.service';
+import { StoreService } from 'src/app/store/store.service';
+import { presets } from '../../../presets'
 
 @Component({
   selector: 'app-library-container',
@@ -13,11 +14,23 @@ import { ArrayConfig, StoreService } from 'src/app/store/store.service';
   styleUrl: './library-container.component.scss'
 })
 export class LibraryContainerComponent {
+  public presets = presets;
   private store = inject(StoreService);
-  public array = computed(() =>this.store.arrayConfig().config);
-  public citation = computed(() => this.store.arrayConfig().citation);
+  public libraryIndex = input<number | null>();
 
-  loadPreset(preset: ArrayConfig) {
+  oldIindex = -1
+
+  public loadPresetEffect = effect(() => {
+    const index = this.libraryIndex();
+    if (index === null || index === undefined || index === this.oldIindex) {
+      return;
+    }
+    this.oldIindex = index;
+
+    const preset = this.presets[index!];
+    if (preset) {
       this.store.setConfig(preset);
-  }
+    }
+  });
+  public citation = computed(() => this.store.arrayConfig().citation);
 }
